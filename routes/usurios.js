d1542100815a50a2cf6beace271087dc32c033ff
Lucas Manuel <lucas_sknr.010@live.com>
2021-02-21 02:11:45 -0300

    End Point para las consultas de usuario

--- routes/usurios.js
+++ routes/usurios.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controllers/usuarioController');
-
+const { check } = require('express-validator');
 // /api/users
-router.post('/', usuarioController.crearUsuario);
+router.post('/', [
+    check('nombre', 'El nombre es necesario').not().isEmpty(),
+    check('email', 'Es necesario un email correcto').isEmail(),
+    check('password', 'La contraseña debe ser del almenos 6 caracteres').isLength({min: 6})
+], usuarioController.crearUsuario);
 
 module.exports = router;

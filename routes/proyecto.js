ab2af843f0e66dcf45b7789ab30d52ad39cd3cc7
Lucas Manuel <lucas_sknr.010@live.com>
2021-02-21 02:15:30 -0300

    End Point para las consultas sobre los proyectos

---
+++ routes/proyecto.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const ProyectoController = require('../controllers/proyectController');
+//importo el middleware de autenticacion
+const authMiddleware = require('../middlewares/auth');
+//valido 
+const { check } = require('express-validator');
+
+// /api/proyect
+//creo proyecto
+router.post('/', [
+    check('nombre', 'El nombre es necesario').not().isEmpty()],
+    authMiddleware, 
+    ProyectoController.crearProyecto);
+//obtengo proyectos
+router.get('/',
+    authMiddleware, 
+    ProyectoController.obtenerProyectos);
+
+//actualizo proyecto
+router.put('/:id',
+    authMiddleware, 
+    ProyectoController.actualizarProyecto);
+
+//eliminar un proyecto
+router.delete('/:id',
+    authMiddleware, 
+    ProyectoController.eliminarProyecto);
+
+module.exports = router;

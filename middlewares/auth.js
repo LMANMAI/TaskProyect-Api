869fca81fe703d094d92ffdbeb68d5da9327eac3
Lucas Manuel <lucas_sknr.010@live.com>
2021-02-21 02:13:07 -0300

    Middleware de autenticacion

---
+++ middlewares/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = function(req,res,next){
+    //tengo que leer el token que envio por header
+    //x-auth-token
+    const token = req.header('x-auth-token');
+
+    ////console.log(token);
+    //reviso si no hay token
+    if(!token){
+        return res.status(401).json({ msg: 'No hay token, permiso no valido'});
+    }
+    //valido el token
+    try {
+        const cifrado = jwt.verify(token, process.env.JWTOKEN);
+        req.user = cifrado.user;
+        next();
+    } catch (error) {
+        //console.log(error);
+        res.status(500).json({msg: 'Hubo un problema validando al usuario'});
+    }
+}

9d696e923b09b1f4db2e097373211643a27c24d6
Lucas Manuel <lucas_sknr.010@live.com>
2021-02-21 02:12:33 -0300

    Controlador para el endpoint de usuarios

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,28 +1,48 @@
-//logica del endpoint
-//importo el modelo de usuarui
+//importo el modelo de usuario
 const UserModel = require('../models/Usuario');
-const bcrytjs = require('bcryptjs');
+const bcryptjs = require('bcryptjs');
+const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
 
 exports.crearUsuario = async ( req, res ) => {
+    //reviso si tengo errores de validacion
+    const errores = validationResult(req);
+    if(!errores.isEmpty()){
+        return res.status(400).json({ errores: errores.array() })
+    }
    try {
     //extraigo el usuario y la contraseña para ver que no se creo el mismo usuario
     const {email, password} = req.body;
-    let user = await UserModel.findOne({email});
-    if(user){
+    let userCompare = await UserModel.findOne({email});
+    if(userCompare){
         return res.status(400).json({msg: 'El usuario ya existe'})
-    }
-   
+    }   
     //creo el usuario con el modelo
-    const usuario = new UserModel(req.body);
-     //hasheo el password
-     const salt = await bcrytjs.genSalt(5);
-     usuario.password = await bcrytjs.hash(password, salt);
+    const user = new UserModel(req.body);
 
-    console.log(usuario)
-    await usuario.save();
-    res.json({msg: 'Usuario creado correctamente'})
+     //hasheo el password
+     const salt = await bcryptjs.genSalt(10);
+     user.password = await bcryptjs.hash(password, salt);
+
+    //guardo el usuario
+    await user.save();
+    //Firmo el token para manejar la sesion
+    const payload = {
+        user:{
+            id: user.id
+        }
+    }
+    //Firmo el jwt
+    jwt.sign(payload, process.env.JWTOKEN, {
+        expiresIn: 3600000
+    },(error, token)=>{
+        if(error) throw error;
+
+        res.json({token});
+    });  
+
    } catch (error) {
-       console.log(error);
+       //console.log(error);
        res.status(500).send(error);
    }
 }
